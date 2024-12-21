bb3464793eba146a4831b346c9b67a1e5eec88c7
wijez <viet.info.43@gmail.com>
2024-12-21 20:50:24 +0700

    first commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+import { Lustria } from 'next/font/google'; 
+
+
+ 
+export const inter = Inter({ style: ['italic'], subsets: ['vietnamese'] });
+
+export const lustria = Lustria({
+    weight: ['400'], 
+    style: 'normal',
+  });   
